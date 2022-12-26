f9f928d04ba43e74360f2a06d1e9a46269a42549
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2022-12-25 23:59:53 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // To be more secure: .env file.
-const supabaseURL = "https://rsrsbxkencjpbzkuorlr.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcnNieGtlbmNqcGJ6a3VvcmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM2OTA5NDEsImV4cCI6MTk3OTI2Njk0MX0.e4WqTbqPs5T30_zrkpEoKClCAX5A8kp9ZVWwuqeSuiw";
+const supabaseURL = "";
+const supabaseAnonKey =""
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
